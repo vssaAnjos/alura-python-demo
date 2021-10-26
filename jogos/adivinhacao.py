5ca498ccfc5b1858ef32fafe3b6dc9127f7777b7
vssaAnjos <anjnessa9@gmail.com>
2021-10-26 09:15:24 -0300

    validando input do usuario

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,17 +8,21 @@ rodada=1
 for rodada in range(1,total_de_tentativas + 1):
     #print("Tentativa ",rodada,"de",total_de_tentativas)
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str= input("DIGITE SEU NUMERO: ")
-
+    chute_str= input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
-
     chute = int(chute_str)
+
+    if (chute < 1 or chute < 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o numero")
