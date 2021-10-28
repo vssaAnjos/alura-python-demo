677c4fbdebb3647ae090c0809a585de1c3ae223d
vssaAnjos <anjnessa9@gmail.com>
2021-10-28 10:26:50 -0300

    alterando o numero aleatorio para a funcao randrange

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,7 +4,7 @@ print("***********************************")
 print("Bem vindo ao Jogo de Adivinhação!")
 print("***********************************")
 
-numero_secreto= round(random.random() * 100)
+numero_secreto= random.randrange(1,101)
 total_de_tentativas=3
 rodada=1
 print(numero_secreto)
@@ -15,7 +15,7 @@ for rodada in range(1,total_de_tentativas + 1):
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
-    if (chute < 1 or chute < 100):
+    if (chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
