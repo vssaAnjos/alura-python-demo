9b6a797e981a4587c04c8d28ee8f60152551236f
vssaAnjos <anjnessa9@gmail.com>
2021-12-21 16:59:37 -0300

    add level to game with input of user

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,14 +4,27 @@ print("***********************************")
 print("Bem vindo ao Jogo de Adivinhação!")
 print("***********************************")
 
-numero_secreto= random.randrange(1,101)
-total_de_tentativas=3
-rodada=1
-print(numero_secreto)
-for rodada in range(1,total_de_tentativas + 1):
-    #print("Tentativa ",rodada,"de",total_de_tentativas)
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 3
+rodada = 1
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defini o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+#print(numero_secreto)
+for rodada in range(1, total_de_tentativas + 1):
+    # print("Tentativa ",rodada,"de",total_de_tentativas)
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str= input("Digite um número entre 1 e 100: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
@@ -27,8 +40,8 @@ for rodada in range(1,total_de_tentativas + 1):
         print("Você acertou!")
         break
     else:
-        if(maior):
+        if (maior):
             print("Você errou! Seu chute foi maior que o numero")
-        elif(menor):
+        elif (menor):
             print("Você errou! Seu chute foi menor que o numero")
 print("Fim de jogo!")
