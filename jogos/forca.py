4eeeb392c4953e16d2758652fad56a10f3ffa062
vssaAnjos <anjnessa9@gmail.com>
2022-01-07 10:53:40 -0300

    completing 'jogo da forca'. Finding letters

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,12 +4,18 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "word"
+    palavra_secreta = "banana"
     enforcou = False
     acertou = False
 
     # Enquanto é True e True = não enforcou e não acertou
     while (not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+                index = index + 1
         print("Jogando...")
 
     print("Fim do jogo")
