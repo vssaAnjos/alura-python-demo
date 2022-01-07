d18456e5629865efce7a12bdc63cf192be3fc40a
vssaAnjos <anjnessa9@gmail.com>
2022-01-07 15:45:39 -0300

    add String functions: strip(), upper()

--- jogos/forca.py
+++ jogos/forca.py
@@ -11,12 +11,14 @@ def jogar():
     # Enquanto é True e True = não enforcou e não acertou
     while (not enforcou and not acertou):
         chute = input("Qual a letra? ")
+        chute = chute.strip() # para limpar espaço em branco
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
+            if(chute.upper() == letra.upper()):    # .upper para comparar tudo como maiscula
                 print("Encontrei a letra {} na posição {}".format(letra,index))
                 index = index + 1
-        print("Jogando...")
+        else:
+            print("Não encontrado...")
 
     print("Fim do jogo")
 
