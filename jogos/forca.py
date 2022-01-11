84da6682c24ca652f90b745196b1d5e32f53dc6e
vssaAnjos <anjnessa9@gmail.com>
2022-01-11 15:36:25 -0300

    saving hits in the list

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,20 +5,26 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     # Enquanto é True e True = não enforcou e não acertou
-    while (not enforcou and not acertou):
+    while (not acertou and not enforcou):
         chute = input("Qual a letra? ")
         chute = chute.strip() # para limpar espaço em branco
+
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):    # .upper para comparar tudo como maiscula
-                print("Encontrei a letra {} na posição {}".format(letra,index))
-                index = index + 1
-        else:
-            print("Não encontrado...")
+              #  print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
+
 
     print("Fim do jogo")
 
