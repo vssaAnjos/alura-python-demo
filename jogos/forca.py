23eff2444b47549bb10fbed8f5e2472602fa7f46
vssaAnjos <anjnessa9@gmail.com>
2022-01-19 16:05:52 -0300

    Add: Verification of hits and misses

--- jogos/forca.py
+++ jogos/forca.py
@@ -8,7 +8,7 @@ def jogar():
     letras_acertadas = ["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
-
+    erros = 0
     print(letras_acertadas)
 
     # Enquanto é True e True = não enforcou e não acertou
@@ -16,15 +16,23 @@ def jogar():
         chute = input("Qual a letra? ")
         chute = chute.strip() # para limpar espaço em branco
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):    # .upper para comparar tudo como maiscula
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):    # .upper para comparar tudo como maiscula
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros += 1
 
+        enforcou = (erros == 6)
+        acertou = ("_" not in letras_acertadas)
         print(letras_acertadas)
 
-
+    if (acertou):
+        print("Você acertou!")
+    else:
+        print("Você perdeu!")
     print("Fim do jogo")
 
 if (__name__ == "__main__"):
